589c9bb59d883e524f65c89da9b71b818129f624
yavuzfeti <yavuzfeti@outlook.com>
2023-10-17 13:13:44 +0300

    yedek

--- lib/main.dart
+++ lib/main.dart
@@ -1,81 +1,40 @@
+import 'dart:async';
+import 'dart:isolate';
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 import 'package:flutter_local_notifications/flutter_local_notifications.dart';
-import 'package:signalr_core/signalr_core.dart';
 import 'package:tester/BackgroundFront.dart';
-import 'package:tester/BackgroundTask.dart';
-import 'package:workmanager/workmanager.dart';
 
-Workmanager work = Workmanager();
-
-@pragma('vm:entry-point')
-void callbackDispatcher() {
-  work.executeTask((task, inputData) async
-  {
-    int index = 1;
-
-    final hubConnection = HubConnectionBuilder().withUrl('https://kursdefteri.com.tr/ip-hub').build();
-
-    bildirim({required String mesage}) async
-    {
-      bool izin = true;
-      if(izin)
-      {
-        await FlutterLocalNotificationsPlugin().show(
-          index,
-          "Bildirim başlığı",
-          mesage,
-          const NotificationDetails(
-            android: AndroidNotificationDetails(
-              "kitx",
-              "kitx_channel",
-              priority: Priority.high,
-              importance: Importance.high,
-              icon: "@mipmap/ic_launcher",
-              color: Colors.blue,
-            ),
-          ),
-        );
-        index++;
-        print("\nBİLDİRİM\n: $mesage");
-      }
-    }
-
-    listenToNotificationStart() async
-    {
-      if (hubConnection.state == HubConnectionState.disconnected)
-      {
-        await hubConnection.start();
-        hubConnection.on('QrCodeRead', (arguments)
-        {
-          bildirim(mesage: arguments.toString());
-        });
-      }
-    }
-    print('çalışmaya başladı');
-    listenToNotificationStart();
-    return Future.value(true);
-  });
-}
-
-void main()
+void main() async
 {
   WidgetsFlutterBinding.ensureInitialized();
+
+  BackgroundIsolateBinaryMessenger.ensureInitialized(Isolate.currentRootIsolate.token);
+
+  final isolate = await Isolate.spawn((message)
+  {
+    int index = 0;
+    Timer.periodic(const Duration(seconds: 2), (timer) {
+      FlutterLocalNotificationsPlugin().show(
+        index,
+        "Bildirim başlığı",
+        "Boş",
+        const NotificationDetails(
+          android: AndroidNotificationDetails(
+            "kitx",
+            "kitx_channel",
+            priority: Priority.high,
+            importance: Importance.high,
+            icon: "@mipmap/ic_launcher",
+            color: Colors.blue,
+          ),
+        ),
+      );
+      index++;
+    });
+  }, null);
+
   //Background.listenToNotificationStart();
-
-  // work.initialize(callbackDispatcher);
-  // work.registerOneOffTask(
-  //   "gorev",
-  //   "gorew",
-  //     constraints: Constraints(
-  //         networkType: NetworkType.connected,
-  //         requiresBatteryNotLow: true,
-  //         requiresCharging: true,
-  //         requiresDeviceIdle: true,
-  //         requiresStorageNotLow: true
-  //     ),
-  // );
-
-  Background.listenToNotificationStart();
 
   runApp(
       MaterialApp(
